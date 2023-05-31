a4aa5995b9a4c86c40971f770b0d5d5af054fefa
Maycol Perez <maycoldanielp@gmail.com>
2023-05-31 17:36:08 -0400

    FIX: solved docs accessing images

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -21,7 +21,7 @@ export const HeroCard = ({
   characters,
 }) => {
 
-  const heroImageUrl = `/heroes/${ id }.jpg`;
+  const heroImageUrl = `./heroes/${ id }.jpg`;
 
   // const charactersByHero = (<p>{ characters }</p>);
 
